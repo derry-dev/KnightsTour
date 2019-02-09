034b2c0ca900dbadfa2b6bb17420fe94270159aa
Derry Leng <derry.leng@gmail.com>
2019-02-09 16:59:34 +0000

    Correct Warnsdorf
    Corrected warnsdorf_tour to frodsnraw_tour.
    Added warnsdorf_tour.

--- tour.py
+++ tour.py
@@ -2,7 +2,7 @@ import csv
 from datetime import datetime
 import random
 
-total_attempts = 10000 # Set number of games to play
+total_attempts = 50000 # Set number of games to play
 board_size = 8 # Set size of board
 start_position = [0,0] # Set start position
 tour_type = "warnsdorf" # Choose type of tour to take
@@ -29,11 +29,18 @@ def random_tour(position):
         move_history.append(position)
         position = random.choice(valid_moves(position))
 
-def warnsdorf_tour(position):
+def frodsnraw_tour(position):
     while len(valid_moves(position)) > 0:
         move_history.append(position)
         moves_rank = [len(valid_moves(x)) for x in valid_moves(position)]
         moves_choice = random.choice([i for i, j in enumerate(moves_rank) if j == max(moves_rank)])
+        position = valid_moves(position)[moves_choice]
+
+def warnsdorf_tour(position):
+    while len(valid_moves(position)) > 0:
+        move_history.append(position)
+        moves_rank = [len(valid_moves(x)) for x in valid_moves(position)]
+        moves_choice = random.choice([i for i, j in enumerate(moves_rank) if j == min(moves_rank)])
         position = valid_moves(position)[moves_choice]
 
 headers = [["Attempt", "Num_Moves", "Progress", "Moves"]]
@@ -47,6 +54,8 @@ with open(filename, "w") as file:
         move_history = []
         if tour_type == "random":
             random_tour(position)
+        elif tour_type == "frodsnraw":
+            frodsnraw_tour(position)
         elif tour_type == "warnsdorf":
             warnsdorf_tour(position)
         row = [[i+1, len(move_history), "{}%".format(len(move_history)/(board_size**2)*100), move_history]]
