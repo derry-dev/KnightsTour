07cc0b9842660020609cc07ebe00ce2566400bbc
Derry Leng <derry.leng@gmail.com>
2019-02-09 11:59:10 +0000

    Warnsdorf & Visualisation
    Added Warnsdorf tours.
    Added tour visualisation module.

--- tour.py
+++ tour.py
@@ -1,11 +1,11 @@
-import random
 import csv
 from datetime import datetime
+import random
 
 total_attempts = 10000 # Set number of games to play
 board_size = 8 # Set size of board
 start_position = [0,0] # Set start position
-tour_type = "random" # Choose type of tour to take
+tour_type = "warnsdorf" # Choose type of tour to take
 
 def valid_moves(position):
     x = [[position[0]-1,position[1]-2],
@@ -29,6 +29,13 @@ def random_tour(position):
         move_history.append(position)
         position = random.choice(valid_moves(position))
 
+def warnsdorf_tour(position):
+    while len(valid_moves(position)) > 0:
+        move_history.append(position)
+        moves_rank = [len(valid_moves(x)) for x in valid_moves(position)]
+        moves_choice = random.choice([i for i, j in enumerate(moves_rank) if j == max(moves_rank)])
+        position = valid_moves(position)[moves_choice]
+
 headers = [["Attempt", "Num_Moves", "Progress", "Moves"]]
 timestamp = "".join(str(x) for x in str(datetime.now().strftime("%Y%m%d%H%M%S")))
 filename = "tour_{}_size{}_{}.csv".format(timestamp, board_size, tour_type)
@@ -40,6 +47,8 @@ with open(filename, "w") as file:
         move_history = []
         if tour_type == "random":
             random_tour(position)
+        elif tour_type == "warnsdorf":
+            warnsdorf_tour(position)
         row = [[i+1, len(move_history), "{}%".format(len(move_history)/(board_size**2)*100), move_history]]
         writer.writerows(row)
     file.close()
